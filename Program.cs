6216a86ac071fae86ae7b9abf91230d63d39ae76
Добрин Дмитрий <zatoichi.cadzicu@gmail.com>
2021-12-28 12:39:57 +0300

    add metod secondArray

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// New massive
+﻿// Метод для создания массива с заданным числом элементов
 
 int[] newArray(int count)
 {
@@ -10,7 +10,7 @@ int[] newArray(int count)
     return array;
 }
 
-//Print Array
+//Метод печати массива
 
 string PrintArray(int[] array)
 {
@@ -22,11 +22,12 @@ string PrintArray(int[] array)
     return result;
 }
 
-//Create new array
+//Создаем новый массив 
 
 int[] test = newArray(6);
 
-//
+//Считаем число четных элементов массива
+
 int countChetNumbers(int[] array)
 {
     int temp = 0;
@@ -37,5 +38,30 @@ int countChetNumbers(int[] array)
     return temp;
 }
 
+//Присваеваем переменной число четных элементов для создания нового массива
+int count = countChetNumbers(test);
+
+//Заполняем второй массив четными элементами первого
+int[] SecondArray(int[] array)
+{
+    int j = 0;
+    int[] second = new int[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            second[j] = array[i];
+            j++;
+        }
+    }
+    return second;
+}
+
+//Печатаем первый массив
 Console.WriteLine("[" + PrintArray(test) + "]");
-Console.WriteLine(countChetNumbers(test));
+
+//Печатаем число четных лементов в массиве
+Console.WriteLine("Число четных элементов в массиве - " + count);
+
+//Печатаем новый массив
+Console.WriteLine("[" + PrintArray(SecondArray(test)) + "]");
